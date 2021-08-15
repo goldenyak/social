91b907b0ddcd0c5378742444cad84cddd0515f81
goldenyak <goldenyakegor@gmail.com>
2021-08-15 23:44:27 +0300

    second commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -18,6 +18,7 @@ function App(props: any) {
                 <div className="content_wrapper">
                     <div className="nav">
                         <Search title={'Поиск'}/>
+                        <Search title={'Поиск'}/>
                         <Navigation/>
                     </div>
                     <div className="profile">
