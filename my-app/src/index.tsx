c440cd6a71e218a280e9ea5afaa44bf337a3e1da
goldenyak <goldenyakegor@gmail.com>
2021-09-02 21:55:17 +0300

    add functions

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state, {addPost} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = () => {
-
     ReactDOM.render(
-        <React.StrictMode>
+        <BrowserRouter>
             <App state={state} addPost={addPost}/>
-        </React.StrictMode>,
+        </BrowserRouter>,
         document.getElementById('root')
     );
 }
